d583e0c7424d9ce6a8eeff2e2e688ba129440121
kingsleychino <kingsleychinono@gmail.com>
2019-10-27 01:26:35 +0800

    committ

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('working...');
+const shareArticle = document.querySelector('.article-share');
+const faIcon = document.querySelectorAll('.fa-fab');
+
+shareArticle.addEventListener('mouseover', () => {
+  faIcon.forEach(icon => (icon.style.display = 'block'));
+});
+
+shareArticle.addEventListener('mouseout', () => {
+  faIcon.forEach(icon => (icon.style.display = 'none'));
+});
